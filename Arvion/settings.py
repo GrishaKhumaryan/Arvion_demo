ee81e7530105d93f01b9865a8245e2537fddbeb8
Karen042009 <karenpoghosyan242@gmail.com>
2025-07-03 13:23:16 +0400

    Move build.sh to root directory

--- Arvion/settings.py
+++ Arvion/settings.py
@@ -14,10 +14,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG ռեժիմը կարդում ենք միջավայրի փոփոխականից։ Render-ում սա կլինի False։
 DEBUG = os.environ.get("DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ["https://arvion-demo.onrender.com", "18.156.158.53"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
