2d2f1819faecc74d2efa143fa6da9a1ab505eaca
Karen042009 <karenpoghosyan242@gmail.com>
2025-07-03 14:38:30 +0400

    Optimize imports in views.py for Render

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 pip install -r requirements.txt
-# Կիրառում ենք միգրացիաները՝ անտեսելով սխալները և նշելով դրանք որպես կատարված
-python manage.py collectstatic --no-input
-python manage.py migrate --fake
+
+python manage.py collectstatic --no manage.py migrate` հրամանը, հավանաբար, նայում է `django_migrations` աղյուսակին (որը `--fake`-ը լրացրել էր) և մտածում է. «Ամեն ինչ արդեն միգրացված է, անելիք չունեմ»։ Արդյունքում, այն չի ստեղծում բացակայող `django_session-input
+
+# Սա կփորձի կիրառել բոլոր միգրացիաները
+python manage.py migrate
+
+# Սա կոնկրետ կստիպի ստեղծել sessions աղյուսակը, եթե այն բացակայում է
+python manage.py migrate sessions
