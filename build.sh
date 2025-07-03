9776a034f521b89a93e0916db504f1ecc51e42d7
Karen042009 <karenpoghosyan242@gmail.com>
2025-07-03 14:42:33 +0400

    Fix build.sh and reset database

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
+\#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no manage.py migrate` հրամանը, հավանաբար, նայում է `django_migrations` աղյուսակին (որը `--fake`-ը լրացրել էր) և մտածում է. «Ամեն ինչ արդեն միգրացված է, անելիք չունեմ»։ Արդյունքում, այն չի ստեղծում բացակայող `django_session-input
-
-# Սա կփորձի կիրառել բոլոր միգրացիաները
+python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Սա կոնկրետ կստիպի ստեղծել sessions աղյուսակը, եթե այն բացակայում է
-python manage.py migrate sessions
